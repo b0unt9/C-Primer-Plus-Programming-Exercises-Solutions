b20de450143b5a0f7526cad53b64c2eb70b6494f
SH <40918958+jmt4k8awngdw1rki3pln@users.noreply.github.com>
2020-09-15 08:55:34 +0900

    Chapter 02

---
+++ Chapter 02 C 첫걸음/프로그래밍 연습 2-6.c
@@ -0,0 +1,18 @@
+/* 프로그래밍 연습 2-6 */
+#include <stdio.h>
+
+int main()
+{
+	int toes = 10;
+	int toes_twice;
+	int toes_squared;
+
+	toes_twice = 2 * toes;
+	toes_squared = toes * toes;
+	
+	printf("toes: %d\n", toes);
+	printf("twice toes: %d\n", toes_twice);
+	printf("toes squared: %d\n", toes_squared);
+	
+	return 0;
+}
