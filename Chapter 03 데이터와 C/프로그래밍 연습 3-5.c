95fdfb9f15d55aa4960fc3507580ce7657ab0762
SH <40918958+jmt4k8awngdw1rki3pln@users.noreply.github.com>
2020-09-15 08:56:20 +0900

    Chapter 03

---
+++ Chapter 03 데이터와 C/프로그래밍 연습 3-5.c
@@ -0,0 +1,13 @@
+/* 프로그래밍 연습 3-5 */
+#include <stdio.h>
+
+int main()
+{
+	unsigned int secound = 31560000;
+	unsigned int age;
+
+	scanf("%u", &age);
+	printf("%u\n", secound * age);
+
+	return 0;
+}
