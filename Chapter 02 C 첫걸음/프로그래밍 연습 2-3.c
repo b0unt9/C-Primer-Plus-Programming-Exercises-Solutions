3d0d3ade82fa07fffc4bdb4e3259c18a8c62ff22
SH <40918958+jmt4k8awngdw1rki3pln@users.noreply.github.com>
2020-09-15 08:55:25 +0900

    Chapter 02

---
+++ Chapter 02 C 첫걸음/프로그래밍 연습 2-3.c
@@ -0,0 +1,13 @@
+/* 프로그래밍 연습 2-3 */
+#include <stdio.h>
+
+int main()
+{
+	int years, days;
+	
+	years = 20;
+	days = years * 365;
+	printf("An age of %d years is %d days.\n", years, days);
+	
+	return 0;
+}
