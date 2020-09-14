5a3b47edeb0360627c1dc88fa3ca9985c1d0875d
SH <40918958+jmt4k8awngdw1rki3pln@users.noreply.github.com>
2020-09-15 08:55:31 +0900

    Chapter 02

---
+++ Chapter 02 C 첫걸음/프로그래밍 연습 2-5.c
@@ -0,0 +1,29 @@
+/* 프로그래밍 연습 2-5 */
+#include <stdio.h>
+
+void br();
+void ic();
+
+int main()
+{
+	br();
+	printf(", ");
+	ic();
+	printf("\n");
+	ic();
+	printf("\n");
+	br();
+	printf("\n");
+	
+	return 0;
+}
+
+void br()
+{
+	printf("Brazil, Russia");
+}
+
+void ic()
+{
+	printf("India, China");
+}
