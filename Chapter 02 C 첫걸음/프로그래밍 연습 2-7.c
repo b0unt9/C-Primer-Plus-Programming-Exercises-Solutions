87f8f5d36f7d340e631b8f83f1f468922cc80a89
SH <40918958+jmt4k8awngdw1rki3pln@users.noreply.github.com>
2020-09-15 08:55:36 +0900

    Chapter 02

---
+++ Chapter 02 C 첫걸음/프로그래밍 연습 2-7.c
@@ -0,0 +1,24 @@
+/* 프로그래밍 연습 2-7 */
+#include <stdio.h>
+
+void smile();
+
+int main()
+{
+	smile();
+	smile();
+	smile();
+	printf("\n");
+	smile();
+	smile();
+	printf("\n");
+	smile();
+	printf("\n");
+	
+	return 0;
+}
+
+void smile()
+{
+	printf("Smile!");
+}
